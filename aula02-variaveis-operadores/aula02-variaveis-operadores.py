7dad5fbef4c14c9f3056ea7762ea526267ebadd9
rhuanpcarreri <rhuanpcarreri>
2026-03-16 22:43:59 -0300

    aula 02

---
+++ aula02-variaveis-operadores/aula02-variaveis-operadores.py
@@ -0,0 +1,28 @@
+print("olá mundo")
+
+print(7 + 4)
+print("7 + 4")
+pint ("7" + "4") # CONCATENA STRINGS
+
+# Comentários de 1 linha no python
+
+''' 
+     comentários de 
+     múltiplas
+     linhas
+     '''
+
+# VARIÁVEIS
+
+nome = "Rhuan"
+idade = 17
+peso = 75.4
+
+print(nome, idade, peso)
+
+# INPUT - SIMULAR UM FORMULÁRIO NO CMD
+nome = input("digite seu nome: ")
+idade =int(input("digite sua idade: "))
+peso = float(input("digite seu peso: "))
+
+print(nome, idade, peso)
---
+++ aula02-variaveis-operadores/desafios.py
@@ -0,0 +1,9 @@
+name = input("digite seu nome")
+print ("seja bem-vindo",name)
+
+dia = input("digite apenas  dia do seu nascimento")
+mes = input("digite apenas  mes do seu nascimento")
+ano = input("digite apenas  ano do seu nascimento")
+print(f"sua data de aniversário é{dia}/{mes}/{ano}")
+
+
